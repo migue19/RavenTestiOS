42d246a24c6084e13a645818ef40dd1fb9415c46
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2025-10-21 13:48:45 -0600

    Initial Commit

---
+++ RavenTestiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RavenTestiOS
+//
+//  Created by Miguel Mexicano Herrera on 21/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RavenTestiOSTests/RavenTestiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RavenTestiOSTests.swift
+//  RavenTestiOSTests
+//
+//  Created by Miguel Mexicano Herrera on 21/10/25.
+//
+
+import Testing
+@testable import RavenTestiOS
+
+struct RavenTestiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
